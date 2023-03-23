547ff5bed86843761ce425598daf95b2954ef533
rwieczor <rwieczor@gmail.com>
2023-03-23 12:50:39 +0100

    Update ICT_code.R

--- ICT_code.R
+++ ICT_code.R
@@ -3,12 +3,10 @@
 #
 # This file contains R functions implementing the estimators described  
 # in the paper: Kowalczyk B., Niemiro W., Wieczorkowski R.:
-
-#   "Item count technique based on two different treatment groups"
-#   (Item count technique with continuous auxiliary variable)
-# (submitted to Statistical Methods in Medical Research)
-#
-# actualization date: 02.05.2017
+# ITEM COUNT TECHNIQUE WITH A CONTINUOUS OR COUNT CONTROL VARIABLE FOR ANALYZING SENSITIVE QUESTIONS IN SURVEYS,
+#  *Journal of Survey Statistics and Methodology* (2021) 00, 1–23,
+#  https://doi.org/10.1093/jssam/smab043
+# actualization date: 23.03.2023
 #
 
 
